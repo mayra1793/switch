145e6514ec547b63807d42a1360cc60487c6555d
Mayra Alvarez <mayra.alvarez.velasquez@gmail.com>
2016-08-26 17:55:41 -0500

    agregando archivos

---
+++ app1.js
@@ -0,0 +1,13 @@
+var nombrePersona = parseInt(prompt("menu: \n"+ 
+	"1.suma\n" +
+	"2.Resta\n" +
+	"3.Multiplicacion\n" +
+	"4.División\n" +
+	"5.Mayor que\n" +
+	"6.Porcentaje"));
+ 
+ switch(operación){
+ 	case 1:
+ 	 var entrada1 = prompt("Operadores Matematicos");
+ 	 var entrada1 = prompt("Operadores Relacionales");
+ }
